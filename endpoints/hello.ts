c411a47a5159e65efb11207214171bae0245f585
WilsonNet <wilsonpsn@hotmail.com>
2022-05-13 17:26:59 -0300

    Update hello.ts

--- endpoints/hello.ts
+++ endpoints/hello.ts
@@ -5,7 +5,7 @@ type Handler = (req: Request, res: Response) => Response | Promise<Response>
 
 const handleGet: Handler = async req => {
 // hello
-  return responseFromJson({hello: "world githkub sssdors aasw cigygygyasel!!"})
+  return responseFromJson({hello: "world githkub sssdors aasw ciggyasel!!"})
 }
 
 const handlers: Record<string, Handler> = {
